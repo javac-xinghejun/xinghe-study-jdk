5fe5eb109b7064de03ab5552b4d66cac92a88e07
xinghe <xinghe@dian.so>
2022-04-24 15:25:52 +0800

    Initial commit

---
+++ src/test/collection/ArrayListTest.java
@@ -0,0 +1,12 @@
+package test.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListTest {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        System.out.println(list);
+    }
+}
