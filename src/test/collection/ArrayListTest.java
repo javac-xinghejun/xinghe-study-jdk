6e22aac57b96671826ad1f9b6046c427e5ed8396
星河 <hechunrun@foxmail.com>
2022-12-30 10:26:18 +0800

    feat(ArrayList): 添加doc
    1. 添加test

--- src/test/collection/ArrayListTest.java
+++ src/test/collection/ArrayListTest.java
@@ -12,6 +12,11 @@ public class ArrayListTest {
         list.add(4);
         list.add(5);
         list.add(6);
+        list.add(6);
+        list.add(6);
+        list.add(6);
+        list.add(6);
+        list.add(6);
         System.out.println(list);
 
         list.remove(2);
